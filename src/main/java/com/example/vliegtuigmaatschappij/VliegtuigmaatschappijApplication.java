6688a66a239c70cedbff9b7bdeb55c401cf2dfd3
Bram de Gooijer <degooijerbram@gmail.com>
2022-10-20 11:19:46 +0200

    Initial commit

---
+++ src/main/java/com/example/vliegtuigmaatschappij/VliegtuigmaatschappijApplication.java
@@ -0,0 +1,13 @@
+package com.example.vliegtuigmaatschappij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VliegtuigmaatschappijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VliegtuigmaatschappijApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vliegtuigmaatschappij/VliegtuigmaatschappijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vliegtuigmaatschappij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VliegtuigmaatschappijApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
