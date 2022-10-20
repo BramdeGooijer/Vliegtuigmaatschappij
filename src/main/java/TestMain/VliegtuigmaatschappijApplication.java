479a511abda4917fd61e4627b5881e450e1c4396
Bram de Gooijer <degooijerbram@gmail.com>
2022-10-20 12:03:01 +0200

    testmain package

--- src/main/java/TestMain/VliegtuigmaatschappijApplication.java
+++ src/main/java/TestMain/VliegtuigmaatschappijApplication.java
@@ -1,4 +1,4 @@
-package com.example.vliegtuigmaatschappij;
+package TestMain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
