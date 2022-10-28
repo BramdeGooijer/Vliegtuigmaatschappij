421dc2d7663160e5adbc6bd6387ed75904b59d98
Bram <degooijerbram@gmail.com>
2022-10-28 18:04:24 +0200

    kinda working hibernate system

--- src/main/java/vliegtuigmaatschappij/domain/Vliegtuig.java
+++ src/main/java/vliegtuigmaatschappij/domain/Vliegtuig.java
@@ -1,8 +1,27 @@
 package vliegtuigmaatschappij.domain;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "vliegtuig")
 public class Vliegtuig {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long vliegtuig_id;
+
     private String vliegtuigtype;
+
     private int stoelenKlas1;
+
     private int stoelenBusiness;
+
     private int stoelenEconomy;
+
+    @OneToMany(mappedBy = "vliegtuig", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Vlucht> alleVluchten = new ArrayList<>();
+
+    public Vliegtuig() {
+
+    }
 }
--- src/main/java/vliegtuigmaatschappij/domain/Vlucht.java
+++ src/main/java/vliegtuigmaatschappij/domain/Vlucht.java
@@ -1,19 +1,34 @@
 package vliegtuigmaatschappij.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@Entity
+@Entity(name = "vlucht")
 public class Vlucht {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long vlucht_id;
+
+    @OneToOne(fetch = FetchType.LAZY)
     private VliegRoute vliegRoute;
-    private List<Boeking> boekingen = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "vliegtuig_id")
     private Vliegtuig vliegtuig;
+
+    @OneToMany(mappedBy = "vlucht", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Boeking> boekingen = new ArrayList<>();
+
     private LocalDateTime vertrekdatum;
+
     private LocalDateTime aankomstdatum;
+
     private double prijsKlas1;
+
     private double prijsBusiness;
+
     private double prijsEconomy;
 }
