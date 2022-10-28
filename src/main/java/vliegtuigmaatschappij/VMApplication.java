4d455798bd56a136247435113ec6dbe656b8da4c
JefrDev <Jeroen.fredriksz@gmail.com>
2022-10-28 18:10:38 +0200

    VMRepository testing

--- src/main/java/vliegtuigmaatschappij/VMApplication.java
+++ src/main/java/vliegtuigmaatschappij/VMApplication.java
@@ -1,7 +1,9 @@
 package vliegtuigmaatschappij;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import vliegtuigmaatschappij.data.VMRepository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,9 +11,14 @@ import java.util.Date;
 
 @SpringBootApplication
 public class VMApplication {
+    public static VMRepository vmRepositoryStatic;
 
     public static void main(String[] args) {
         SpringApplication.run(VMApplication.class, args);
     }
 
+    @Autowired
+    public void setVmRepositoryStatic(VMRepository vmRepository) {
+        vmRepositoryStatic = vmRepository;
+    }
 }
